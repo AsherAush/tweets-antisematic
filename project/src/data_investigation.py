b45674c3615cdc87da1b89ee6a922accf55d9e8f
asher zeev aush <A0583251203@gmail.com>
2025-08-04 16:36:13 +0300

    The 3 longest tweets and 10 most common words function

--- project/src/data_investigation.py
+++ project/src/data_investigation.py
@@ -30,11 +30,34 @@ class Investigate_data:
             "total": float(self.df['length'].mean())
         }
 
+    def longest_3_tweets(self):
+        # This function returns the three longest tweets in each category.
+        self.antisemite['char_count'] = self.antisemite['Text'].apply(len)
+        self.no_antisemite['char_count'] = self.no_antisemite['Text'].apply(len)
+
+        # Sort the DataFrame by character count and get the top 3 tweets
+        anti_long = self.antisemite.sort_values(by='char_count', ascending=False).head(3)['Text'].tolist()
+        non_anti_long = self.no_antisemite.sort_values(by='char_count', ascending=False).head(3)['Text'].tolist()
+
+        return {
+            "antisemitic": anti_long,
+            "non_antisemitic": non_anti_long
+        }
+
+    def common_words(self):
+        # This function returns the ten most common words in the tweets.
+        full_data = ''.join(self.df['Text'].tolist()).lower().split()
+        word_counter = pd.Series(full_data).value_counts()
+        return {"total": word_counter.head(10).index.to_list()}
+
 
 
 
 
 investigate = Investigate_data(file)
 # investigate = investigate.toral_tweets()
-investigate = investigate.average_length()
+# investigate = investigate.average_length( )
+# investigate = investigate.longest_3_tweets()
+investigate = investigate.common_words()
+
 print(investigate)
