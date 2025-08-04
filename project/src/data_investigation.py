a73d57cf9e6a5510bea740b678220f6622d45615
asher zeev aush <A0583251203@gmail.com>
2025-08-04 12:22:43 +0300

    Average word length function by categories

--- project/src/data_investigation.py
+++ project/src/data_investigation.py
@@ -8,15 +8,33 @@ file = file.load("C:/pycharm/tweets-antisematic/project/data/tweets_dataset.csv"
 class Investigate_data:
     def __init__(self, df):
         self.df = df
+        self.antisemite = df[df['Biased'] == 1].copy()
+        self.no_antisemite = df[df['Biased'] == 0].copy()
 
     def toral_tweets(self):
         #This function returns the number of tweets in each category.
         counts = self.df['Biased'].value_counts()
         return {
-            "antisemitic": int(counts.get(0)),
-            "non_antisemitic": int(counts.get(1)),
+            "antisemitic": int(counts.get(1)),
+            "non_antisemitic": int(counts.get(0)),
             "total": len(self.df)}
 
+    def average_length(self):
+        #This function returns the average length of the tweets at katagoryus.
+        self.antisemite['lengthyas'] = self.antisemite['Text'].apply(lambda x: len(x.split()))
+        self.no_antisemite['lengthno'] = self.no_antisemite['Text'].apply(lambda x: len(x.split()))
+        self.df['length'] = self.df['Text'].apply(lambda x: len(x.split()))
+        return {
+            "antisemitic": float(self.antisemite['lengthyas'].mean()),
+            "non_antisemitic": float(self.no_antisemite['lengthno'].mean()),
+            "total": float(self.df['length'].mean())
+        }
+
+
+
+
+
 investigate = Investigate_data(file)
-investigate = investigate.toral_tweets()
+# investigate = investigate.toral_tweets()
+investigate = investigate.average_length()
 print(investigate)
